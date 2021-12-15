a70c6a392a9b97ce2d2ab8243c83e555593b4775
rainishadass <83724972+rainishadass@users.noreply.github.com>
2021-12-15 04:27:21 +0000

    added fizzbuzz func under final.cpp

--- final.cpp
+++ final.cpp
@@ -8,13 +8,24 @@ void isRightTriangle(int a,int b, int c){
   }
 }
 
-
-
-
-
+void fizzbuzz(int n){
+  
+  for (int i=1; i<=n;i=i+1){
+    if (i % 3 == 0 && i % 5 == 0){
+      std::cout << "fizzbuzz\n";
+    } else if (i % 3 == 0) {
+      std::cout << "fizz\n";
+    } else if (i % 5 == 0){
+      std::cout << "buzz\n";
+    } else{
+      std::cout << i << "\n";
+    }
+  }
+}
 
 
 int main(){
-  isRightTriangle(3,4,5);
+  //isRightTriangle(3,4,5);
+  fizzbuzz(21);
   return 0;
 }
